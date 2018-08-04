2b2dce2610c56648331a7ff7168bbe3acd41e8f0
makerXinpeng <30549862+makerXinpeng@users.noreply.github.com>
2018-08-04 18:56:28 +0800

    Add files via upload

---
+++ tube.cpp
@@ -0,0 +1,13 @@
+#include<arduino.h>
+#include"tube.h"
+void Tube::tube_init()
+{
+  for (int i = 6; i < 18; i++)
+    pinMode(i, OUTPUT);
+  pinMode(20, INPUT);
+}
+void Tube::tube_turnoff()
+{
+  for (int i = 6; i < 14; i++)
+    digitalWrite(i, HIGH);
+}
---
+++ tube.h
@@ -0,0 +1,6 @@
+class Tube
+{
+  public:
+  void static tube_init();
+  void static tube_turnoff();
+};
